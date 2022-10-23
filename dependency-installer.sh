7325c73f067ad04047c206869ba7d92b8b5ca043
Brage Skjonborg <brage.skjonborg@hotmail.no>
2022-10-23 21:58:54 +0200

    Update

--- dependency-installer.sh
+++ dependency-installer.sh
@@ -20,16 +20,21 @@ chmod +x /usr/sbin/ngssc
 if [[ $TARGETARCH == "arm"* ]] 
 then 
     curl -L "https://cdn.azul.com/zulu-embedded/bin/zulu17.38.21-ca-jre17.0.5-linux_aarch32hf.tar.gz" -o java.tar.gz
+    echo "Listing out java in PWD"
     ls -lR . | grep java
 else
     curl -L "https://cdn.azul.com/zulu/bin/zulu17.38.21-ca-jre17.0.5-linux_x64.tar.gz" -o java.tar.gz
+    echo "Listing out java in PWD"
     ls -lR . | grep java
 fi
 
+echo "Creating directory"
 mkdir -p /opt/java17
-tar -xzvf ./java.tar.gz --strip-components=1 -C /opt/java17
+
+echo "Uncompressing java.tar.gz"
+echo "Working directory is:" $PWD 
+tar -xzvf java.tar.gz --strip-components=1 -C /opt/java17
 ls -lR /opt/java17
 chmod +x /opt/java17/bin/java
 
 update-alternatives --install /usr/bin/java java /opt/java17/bin/java 100
-rm java.tar.gz
