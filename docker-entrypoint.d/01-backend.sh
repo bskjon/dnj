b5e784f98699deecd3eb6eace2e80033c9860092
Brage Skjonborg <brage.skjonborg@hotmail.no>
2022-10-23 21:00:51 +0200

    Corrected ext

--- docker-entrypoint.d/01-backend.sh
+++ docker-entrypoint.d/01-backend.sh
@@ -2,7 +2,7 @@
 
 java --version
 
-if [ -f "/usr/share/backend.java" ]; then
+if [ -f "/usr/share/backend.jar" ]; then
     echo "Found backend file"
     termination(){
         kill -SIGINT $PID_SERVER
@@ -11,11 +11,11 @@ if [ -f "/usr/share/backend.java" ]; then
 
     trap 'termination' SIGTERM
 
-    java -jar /usr/share/backend.java &
+    java -jar /usr/share/backend.jar &
     PID_SERVER=$!
 
     exec "$@"
 else
-    echo "Could not find backend in /usr/share/backend.java"
+    echo "Could not find backend in /usr/share/backend.jar"
     echo "Skipping.."
 fi
