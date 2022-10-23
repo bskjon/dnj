c6ef9e3eeb772497efc7505ec6820adaf105599b
Brage Skjonborg <brage.skjonborg@hotmail.no>
2022-10-23 21:43:10 +0200

    Updated..

--- dependency-installer.sh
+++ dependency-installer.sh
@@ -20,14 +20,15 @@ chmod +x /usr/sbin/ngssc
 if [[ $TARGETARCH == "arm"* ]] 
 then 
     curl -L "https://cdn.azul.com/zulu-embedded/bin/zulu17.38.21-ca-jre17.0.5-linux_aarch32hf.tar.gz" -o java.tar.gz
-    echo ls -la 
+    du -a .
 else
     curl -L "https://cdn.azul.com/zulu/bin/zulu17.38.21-ca-jre17.0.5-linux_x64.tar.gz" -o java.tar.gz
-    echo ls -la 
+    du -a .
 fi
 
 mkdir -p /opt/java17
 tar -xzvf ./java.tar.gz --strip-components=1 -C /opt/java17
+du -a /opt/java17
 chmod +x /opt/java17/bin/java
 
 update-alternatives --install /usr/bin/java java /opt/java17/bin/java 100
