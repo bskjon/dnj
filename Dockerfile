a05dea44f8f1b6912a293c8b471142cdf3c30237
Brage Skjonborg <brage.skjonborg@hotmail.no>
2022-10-23 23:00:24 +0200

    Fixed build

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,16 @@
 FROM nginx:latest
 
 ARG TARGETARCH
-ENV AM_I_IN_A_DOCKER_CONTAINER True
 
+# RUN \
+#     apt update && \
+#     apt upgrade -y && \
+#     apt install -y --no-install-recommends
 
-COPY dependency-installer.sh /dependency-installer.sh
-RUN chmod +x /dependency-installer.sh
-RUN /dependency-installer.sh
+COPY installer.sh /installer.sh
+RUN chmod +x /installer.sh
+# RUN ls -la .
+RUN /installer.sh
 
 COPY nginx.conf /etc/nginx/nginx.conf
 
@@ -16,8 +20,10 @@ RUN chmod +x /docker-entrypoint.d/999-ngssc.sh
 RUN chmod +x /docker-entrypoint.d/01-backend.sh
 
 # Cleanup
-RUN rm /dependency-installer.sh
+# RUN rm /dependency-installer.sh
 # RUN rm java.tar.gz
+
+ENV AM_I_IN_A_DOCKER_CONTAINER True
 
 EXPOSE 8080
 EXPOSE 80
---
+++ installer.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+mkdir -p /usr/sbin/
+mkdir -p /opt/java17
+
+version=$(basename $(curl -fs -o/dev/null -w %{redirect_url} https://github.com/kyubisation/angular-server-side-configuration/releases/latest))
+
+echo "Building with => " $TARGETARCH
+
+if [[ $TARGETARCH == "arm"* ]] 
+then 
+    echo "Detected Target Arch ARM" $TARGETARCH >> /usr/sbin/ngssc.info
+    printf "\nDownloading Ngssc\n"
+    curl -L "https://github.com/bskjon/angular-server-side-configuration/releases/download/master/ngssc_arm_32bit" -o /usr/sbin/ngssc
+
+    printf "\n\nDownloading Java\n"
+    curl -L "https://cdn.azul.com/zulu-embedded/bin/zulu17.38.21-ca-jre17.0.5-linux_aarch32hf.tar.gz" -o /java.tar.gz
+
+else
+    echo "Detected Target Arch x86_x64 (default)" $TARGETARCH >> /usr/sbin/ngssc.info
+    printf "\nDownloading Ngssc\n"
+    curl -L "https://github.com/kyubisation/angular-server-side-configuration/releases/download/$version/ngssc_64bit" -o /usr/sbin/ngssc
+
+    printf "\n\nDownloading Java\n"
+    curl -L "https://cdn.azul.com/zulu/bin/zulu17.38.21-ca-jre17.0.5-linux_x64.tar.gz" -o /java.tar.gz
+fi
+
+chmod +x /usr/sbin/ngssc
+
+tar -xzvf java.tar.gz --strip-components=1 -C /opt/java17
+
+if [ -z "$(ls -A -- "/opt/java17")" ]; then
+    printf "\n\nJava folder is empty!\n"
+    exit 1
+fi
+chmod +x /opt/java17/bin/java
+
+update-alternatives --install /usr/bin/java java /opt/java17/bin/java 100
